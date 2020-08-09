5d7e29f1120e05e3c6c17477e3a0d26879e062e1
Kelley Muro <kelleymuro@gmail.com>
2020-08-08 17:12:02 -0700

    Initial Commit

---
+++ Demo1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Demo1
+//
+//  Created by Kelley Muro on 8/8/20.
+//  Copyright © 2020 Kelley Muro. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
